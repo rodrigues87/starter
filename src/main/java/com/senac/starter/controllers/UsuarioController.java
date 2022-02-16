15867a86a923aeb08db3348635d411d4527f00e4
sala19a <pedroh.mix>
2022-02-16 20:07:06 -0300

    Atualizando a tela de usuarios

--- src/main/java/com/senac/starter/controllers/UsuarioController.java
+++ src/main/java/com/senac/starter/controllers/UsuarioController.java
@@ -5,10 +5,13 @@ import com.senac.starter.repositorys.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/usuarios")
@@ -25,5 +28,17 @@ public class UsuarioController {
         modelAndView.addObject("usuarios", usuarios);
         return modelAndView;
 
+    }
+
+    @GetMapping("/{id}")
+    public ModelAndView detalharUsuario(@PathVariable Long id){
+
+        Optional<Usuario> usuario = usuarioRepository.findById(id);
+
+        ModelAndView modelAndView  = new ModelAndView("usuario-detalhe");
+
+        modelAndView.addObject("usuario",usuario);
+
+        return modelAndView;
     }
 }
