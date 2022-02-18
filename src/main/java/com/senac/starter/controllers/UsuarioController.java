0f80fb46b2348f01aeff6624259e0045da0755a8
sala19a <pedroh.mix>
2022-02-17 21:32:55 -0300

    Inserindo novo usuario

--- src/main/java/com/senac/starter/controllers/UsuarioController.java
+++ src/main/java/com/senac/starter/controllers/UsuarioController.java
@@ -4,10 +4,7 @@ import com.senac.starter.models.Usuario;
 import com.senac.starter.repositorys.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -41,6 +38,20 @@ public class UsuarioController {
         modelAndView.addObject("usuario",usuario);
 
         return modelAndView;
+    }
+
+    @GetMapping("/add")
+    public ModelAndView adicionar(){
+        ModelAndView modelAndView = new ModelAndView("usuario-detalhe");
+        Usuario usuario = new Usuario();
+        modelAndView.addObject("usuario", usuario);
+        return modelAndView;
+    }
+
+    @PostMapping("/add")
+    public String adicionarUsuario(Usuario usuario){
+        usuarioRepository.save(usuario);
+        return "redirect:/usuarios";
     }
 
     @GetMapping("/deletar/{id}")
